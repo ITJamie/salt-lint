ac6500644999e9b17a52f00df103a3c603f0d646
Jamie Murphy <jamiemurphyit@gmail.com>
2024-09-02 18:36:44 +0100

    crons should have an identifier

---
+++ tests/unit/TestCronIdRule.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Warpnet B.V.
+
+import unittest
+
+from saltlint.linter.collection import RulesCollection
+from saltlint.rules.CronIdRule import CronIdRule
+from tests import RunFromText
+
+
+GOOD_CMD_STATE = '''
+run_postinstall:
+  cron.present:
+    - name: echo hello
+    - identifier: yes
+'''
+
+BAD_CMD_STATE = '''
+run_postinstall:
+  cron.present:
+    - name: echo hello
+'''
+
+class TestCmdWaitRecommendRule(unittest.TestCase):
+    collection = RulesCollection()
+
+    def setUp(self):
+        self.collection.register(CronIdRule())
+
+    def test_statement_positive(self):
+        runner = RunFromText(self.collection)
+        results = runner.run_state(GOOD_CMD_STATE)
+        import pprint
+        pprint.pprint(results)
+        self.assertEqual(0, len(results))
+
+    def test_statement_negative(self):
+        runner = RunFromText(self.collection)
+        results = runner.run_state(BAD_CMD_STATE)
+        # import pprint
+        # pprint.pprint(results)
+        self.assertEqual(1, len(results))
